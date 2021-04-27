bb7ac405a14875abbd011115d331c1c3adee4290
Debam99 <debamgarai18@gmail.com>
2021-04-28 00:45:13 +0530

    Create cycleInLinkedlist.java

---
+++ LinkedList/cycleInLinkedlist.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+      
+         if(head==null)
+            return false;
+        
+        
+        ListNode slow=head;
+        ListNode fast=head.next;
+       
+        
+        while(slow!=fast){
+            if(fast==null  || fast.next==null){
+                return false;
+            }else{
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            
+        }
+        return true;
+    }
+}
