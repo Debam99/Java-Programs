a431c53ed49175c5b4ac857438d3a0a9dda3d391
Debam99 <debamgarai18@gmail.com>
2021-06-28 20:27:36 +0530

    Add files via upload

---
+++ Recursion/getMazePath.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class getMazePath {
+  public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int m=sc.nextInt();
+
+      ArrayList<String> paths = getPaths(1,1,n,m);
+    System.out.println(paths);
+    } 
+  
+  
+  public static ArrayList<String> getPaths(int sr,int sc,int dr,int dc){
+   if(sr==dr && sc==dc){
+    ArrayList<String> bres=new ArrayList<>();
+    bres.add("");
+    return bres;
+   }
+   
+    ArrayList<String> hpaths=new ArrayList<>();
+    ArrayList<String> vpaths=new ArrayList<>();
+   if(sc<dc){
+     hpaths = getPaths( sr, sc+1, dr, dc);}
+    if(sr<dr){ 
+     vpaths = getPaths( sr+1, sc, dr, dc);}
+    ArrayList<String> paths=new ArrayList<>();
+
+    for(String hpath: hpaths){
+        paths.add("h"+hpath);
+    }
+    for(String vpath: vpaths){
+        paths.add("v"+vpath);
+    }
+
+    return paths;
+    
+}
+}
